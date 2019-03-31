fe66c277b8f038eeeb00cc7388a2ad64f5c11216
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2019-03-31 16:13:50 +0200

    Wrote character creation method

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 
-from player import Player
 from gamedata import GameData
-from json_serialization import save_gamedata, load_gamedata
+from json_serialization import load_gamedata, save_gamedata
+from player import Player
+
 
 def print_bonus_tasks():
     # TODO: insert the tasks you implemented
     bonus_tasks = []
-    print(','.join(str(x) for x in bonus_tasks))
+    print(",".join(str(x) for x in bonus_tasks))
+
 
 def main():
     parser = argparse.ArgumentParser(description="P0 Adventure")
-    parser.add_argument('--savefile', default="game.json",
-        help="The save file. default: 'game.json'")
-    parser.add_argument("--new-game", dest="new_game", default=False, action='store_true',
-        help="Create a new save file.")
-    parser.add_argument("-b", dest="bonus_tasks", default=False, action="store_true", help='enable bonus tasks')
-    parser.add_argument("--print-bonus", dest="print_bonus", default=False, action="store_true", help='print bonus task list and exit')
-    args = parser.parse_args( )
+    parser.add_argument(
+        "--savefile", default="game.json", help="The save file. default: 'game.json'"
+    )
+    parser.add_argument(
+        "--new-game",
+        dest="new_game",
+        default=False,
+        action="store_true",
+        help="Create a new save file.",
+    )
+    parser.add_argument(
+        "-b",
+        dest="bonus_tasks",
+        default=False,
+        action="store_true",
+        help="enable bonus tasks",
+    )
+    parser.add_argument(
+        "--print-bonus",
+        dest="print_bonus",
+        default=False,
+        action="store_true",
+        help="print bonus task list and exit",
+    )
+    args = parser.parse_args()
 
     if args.print_bonus:
         print_bonus_tasks()
@@ -27,5 +48,12 @@ def main():
 
     # your code starts here
 
-if __name__ == '__main__':
+    if args.new_game:
+        player = Player()
+        player.create_new_character()
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from json_serialization import json_class
 
+
 @json_class
 class Player:
     def __init__(self, **player):
@@ -20,3 +21,44 @@ class Player:
         print("  * Defense: {}".format(self.defense))
         print("  * Speed: {}".format(self.speed))
         print()
+
+    def create_new_character(self):
+        print("Welcome to P0 Dungeon Quest character creator!")
+        self.name = input("Enter your name: ")
+        self._assign_points()
+
+    def _assign_points(self):
+        print("You have 100 points to assign to you character.")
+        print(
+            "Start now to assign those Points to your characters attack, defense and speed."
+        )
+        self.attack = int(input("Attack: "))
+        while self.attack <= 0:
+            print("Please input a positive integer")
+            self.attack = int(input("Attack: "))
+
+        self.defense = int(input("Defense: "))
+        while self.defense <= 0:
+            print("Please input a positive integer")
+            self.defense = int(input("Defense: "))
+
+        self.speed = int(input("Speed: "))
+        while self.speed <= 0:
+            print("Please input a positive integer")
+            self.speed = int(input("Speed: "))
+
+        if self.attack + self.defense + self.speed > 100:
+            print(
+                "Sorry, it seems like you spent more than 100 ability points on your character... Try that again!"
+            )
+            print()
+            self._assign_points()
+
+        print("Before you store your character please confirm your stats!")
+        self.print_stats()
+
+        answer = input("Is this correct? (Y/N) ")
+        while answer.lower() not in ["y", "n"]:
+            answer = input("Please enter Y/y for yes or N/n for no!")
+        if answer.lower() == "n":
+            self.create_new_character()
